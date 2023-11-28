98fc0293eaa89dd86c58c4e51621428d38321c24
samar ibrahim <samaribrahim@std.mans.edu.eg>
2023-11-28 07:47:06 +0200

    add libraries

---
+++ Libraries/errorStates.h
@@ -0,0 +1,19 @@
+/*
+ * errorStates.h
+ *
+ *  Created on: 12/10/2023
+ *      Author: samar ibrahim
+ */
+
+#ifndef ERRORSTATES_H_
+#define ERRORSTATES_H_
+
+typedef enum
+{
+	ES_NOK,
+	ES_OK,
+	ES_NULL_POINTER,
+	ES_OUT_OF_RANGE
+}ES_t;
+
+#endif /* ERRORSTATES_H_ */
